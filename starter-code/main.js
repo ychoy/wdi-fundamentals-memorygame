888597a994687d65743328479ba46898fe32657b
ychoy <yanyinchoy@gmail.com>
2017-01-21 07:12:30 -0800

    added alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,13 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
+if (cardOne === cardThree) {
+// Code to be executed if condition1 is true
+  "You found a match!";
+} else if (cardTwo === cardFour) {
+// Code to be executed if condition1 is false and condition2 is true
+  "You found a match!";
+} else {
+// Code to be executed if condition1, condition2, and condition3 are false
+ alert("Sorry, try again.");
+}
