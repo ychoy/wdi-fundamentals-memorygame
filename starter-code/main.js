a64202886a7d4c8bb0e8d46f276ff09e0a4e8794
ychoy <yanyinchoy@gmail.com>
2017-01-21 08:42:22 -0800

    organized code and created dynamic html

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,20 @@
 console.log("JS file is connected to HTML! Woo!")
 
+function createBoard(cards) {
+  for (var i = 0; i < cards.length; i++) {
+    // create a section element ... <section></section>
+    var cardElement = document.createElement('div');
+    // add a class
+    cardElement.className = 'card';
+    // append the area to div, a world element <div> <section class='cards'></section></div>
+
+    //setting HTML tag attributes
+    //<article id="trogdor" class="burninating"></article>
+
+    board.appendChild(cardElement);
+  }
+}
+
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
@@ -13,5 +28,9 @@ if (cardOne === cardThree) {
   "You found a match!";
 } else {
 // Code to be executed if condition1, condition2, and condition3 are false
- alert("Sorry, try again.");
+  alert("Sorry, try again.");
 }
+
+let cards = [ cardOne, cardTwo, cardThree, cardFour ];
+let board = document.getElementById('game-board');
+createBoard(cards)
