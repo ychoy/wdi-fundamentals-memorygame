5ff244790581e9174ac957225c770ee6fbeb0bd2
ychoy <yanyinchoy@gmail.com>
2017-01-21 10:32:03 -0800

    modified matching function, linked cards to images, improved logic with arrays

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,36 +1,54 @@
-console.log("JS file is connected to HTML! Woo!")
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var board = document.getElementById('game-board');
+  function createBoard(cards) {
+    // loop through length of cards
+    for (var i = 0; i < cards.length; i++) {
+      // create a div element which will be used as a card
+      var cardElement = document.createElement('div');
+      // add a class
+      cardElement.className = 'card';
+      // this will set the card's "data-card" to be element of the array
+      // data attributes are meant to store data about an element that is not tied to a style
+      // e.g. "king".
+      cardElement.setAttribute('data-card', cards[i]);
+      //when a card is selected the function isTwoCards will be executed
+      cardElement.addEventListener('click', isTwoCards);
+      // append card to the board
+      board.appendChild(cardElement);
+      board.appendChild(cardElement);
+  }
+}
+// checks to seet if there are cards in play
+  function isTwoCards() {
+  // add card to array of cards in play
+  //'this' hasn't been covered in this pre-work, but
+  //for now, just know it gives you access to the card the user clicked on
+  cardsInPlay.push(this.getAttribute('data-card'));
+    //if you have two cards in play, check for a match
+  console.log(this.getAttribute('data-card'));
+    if (this.getAttribute('data-card') === 'king') {
+      this.innerHTML = "<img src = 'http://i.imgur.com/bnuv5Im.png'>";
+    } else {
+      this.innerHTML = "<img src = 'http://i.imgur.com/v6buNt2.png'>";
+    }
+    //if you have two cards in play check for a match
+    if (cardsInPlay.length === 2)  {
+      // pass the cardsInPlay as an argument to the isMatch function
+      isMatch(cardsInPlay);
+      // clear cards in play array for your next try
+      cardsInPlay = [];
+    }
+  }
 
-function createBoard(cards) {
-  for (var i = 0; i < cards.length; i++) {
-    // create a section element ... <section></section>
-    var cardElement = document.createElement('div');
-    // add a class
-    cardElement.className = 'card';
-    // append the area to div, a world element <div> <section class='cards'></section></div>
-
-    //setting HTML tag attributes
-    //<article id="trogdor" class="burninating"></article>
-
-    board.appendChild(cardElement);
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+  // Code to be executed if condition1 is true
+   alert("You found a match!");
+  } else {
+  // Code to be executed if condition1 is false
+   alert("Sorry, try again.");
   }
 }
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-
-if (cardOne === cardThree) {
-// Code to be executed if condition1 is true
-  "You found a match!";
-} else if (cardTwo === cardFour) {
-// Code to be executed if condition1 is false and condition2 is true
-  "You found a match!";
-} else {
-// Code to be executed if condition1, condition2, and condition3 are false
-  alert("Sorry, try again.");
-}
-
-let cards = [ cardOne, cardTwo, cardThree, cardFour ];
-let board = document.getElementById('game-board');
-createBoard(cards)
+createBoard(cards);
